18a183cf6aa21bdd5a5ff0a85945785fdd16987d
lixinxin <895330766@qq.com>
2018-09-27 17:47:16 +0800

    添加配置

---
+++ src/main/java/com/lixinxinlove/constant/CookieConstant.java
@@ -0,0 +1,12 @@
+package com.lixinxinlove.constant;
+
+/**
+ * cookie常量
+ *
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/lixinxinlove/constant/RedisConstant.java
@@ -0,0 +1,12 @@
+package com.lixinxinlove.constant;
+
+/**
+ * redis常量
+
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    Integer EXPIRE = 7200; //2小时
+}
