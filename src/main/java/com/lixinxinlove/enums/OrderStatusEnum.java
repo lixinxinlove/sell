d2dce153fb444f497192900608770b7453f888dd
lixinxin <895330766@qq.com>
2018-09-28 15:37:45 +0800

    添加模板

--- src/main/java/com/lixinxinlove/enums/OrderStatusEnum.java
+++ src/main/java/com/lixinxinlove/enums/OrderStatusEnum.java
@@ -2,16 +2,12 @@ package com.lixinxinlove.enums;
 
 import lombok.Getter;
 
-/**
- * Created by 廖师兄
- * 2017-06-11 17:12
- */
+
 @Getter
 public enum OrderStatusEnum implements CodeEnum {
     NEW(0, "新订单"),
     FINISHED(1, "完结"),
-    CANCEL(2, "已取消"),
-    ;
+    CANCEL(2, "已取消");
 
     private Integer code;
 
--- src/main/java/com/lixinxinlove/enums/PayStatusEnum.java
+++ src/main/java/com/lixinxinlove/enums/PayStatusEnum.java
@@ -2,17 +2,11 @@ package com.lixinxinlove.enums;
 
 import lombok.Getter;
 
-/**
- * Created by 廖师兄
- * 2017-06-11 17:16
- */
 @Getter
 public enum PayStatusEnum implements CodeEnum {
 
     WAIT(0, "等待支付"),
-    SUCCESS(1, "支付成功"),
-
-    ;
+    SUCCESS(1, "支付成功");
 
     private Integer code;
 
